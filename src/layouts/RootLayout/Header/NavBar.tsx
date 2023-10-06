ab0dec5cb5d8afc5bf83c59fa4802205d71ed10b
Fairuz Ikbar <82931694+fairuzikbar@users.noreply.github.com>
2023-10-06 20:01:14 +0700

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -1,7 +1,10 @@
 import styled from "@emotion/styled"
 import Link from "next/link"
+import { useState } from "react"
 
 const NavBar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const links = [
     { id: 1, name: "About", to: "/about" },
     { id: 2, name: "Resume", to: "/resume" },
@@ -10,10 +13,20 @@ const NavBar: React.FC = () => {
     { id: 5, name: "Contact", to: "/contact" },
     { id: 6, name: "Contact", to: "/contact" },
     { id: 7, name: "Contact", to: "/contact" }
-  ]
+  ];
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <StyledWrapper className="">
-      <ul>
+      <BurgerMenuButton onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? "open" : ""}`} />
+        <div className={`bar ${menuOpen ? "open" : ""}`} />
+        <div className={`bar ${menuOpen ? "open" : ""}`} />
+      </BurgerMenuButton>
+      <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
         {links.map((link) => (
           <li key={link.id}>
             <Link href={link.to}>{link.name}</Link>
@@ -37,4 +50,53 @@ const StyledWrapper = styled.div`
       color: ${({ theme }) => theme.colors.gray11};
     }
   }
-`
+`;
+
+const BurgerMenuButton = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  .bar {
+    width: 30px;
+    height: 3px;
+    background-color: ${({ theme }) => theme.colors.gray11};
+    margin: 4px 0;
+    transition: 0.4s;
+  }
+  &.open {
+    .bar:nth-child(1) {
+      transform: rotate(-45deg) translate(-5px, 6px);
+    }
+    .bar:nth-child(2) {
+      opacity: 0;
+    }
+    .bar:nth-child(3) {
+      transform: rotate(45deg) translate(-5px, -6px);
+    }
+  }
+  
+  @media (max-width: 768px) {
+    display: flex;
+  }
+}
+
+.nav-menu {
+  @media (max-width: 768px) {
+    flex-direction: column;
+    background-color: white;
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 100%;
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.2s ease-in-out;
+    &.open {
+      max-height: 300px; /* Adjust this value as needed */
+    }
+    li {
+      margin: 0;
+      padding: 0.5rem 1rem;
+    }
+  }
+}
