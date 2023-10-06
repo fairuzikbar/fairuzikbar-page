25a10f30aa77533a0b4ddcaf61d471e6dea1a23e
Fairuz Ikbar <82931694+fairuzikbar@users.noreply.github.com>
2023-10-06 20:48:01 +0700

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -84,7 +84,7 @@ const NavMenu = styled.ul`
   }
 
   @media (max-width: 768px) {
-    flex-direction: column; /* Horizontal direction */
+    flex-direction: row; /* Horizontal direction */
     background-color: white;
     position: absolute;
     top: 0; /* Keep the menu at the top */
