74d263c967464fde176928454daad33fbb0f2061
Fairuz Ikbar <82931694+fairuzikbar@users.noreply.github.com>
2023-10-06 17:07:22 +0700

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled"
 import Link from "next/link"
+import { Dropdown } from 'flowbite-react';
 
 const NavBar: React.FC = () => {
   const links = [
@@ -8,18 +9,54 @@ const NavBar: React.FC = () => {
     { id: 3, name: "Test", to: "/test" },
     { id: 4, name: "Contact", to: "/contact" }
   ]
-  return (
-    <StyledWrapper className="">
-      <ul>
-        {links.map((link) => (
-          <li key={link.id}>
-            <Link href={link.to}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </StyledWrapper>
-  )
-}
+//   return (
+//     <StyledWrapper className="">
+//       <ul>
+//         {links.map((link) => (
+//           <li key={link.id}>
+//             <Link href={link.to}>{link.name}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </StyledWrapper>
+//   )
+// }
+
+return (
+    <>
+      {/* Desktop Navbar */}
+      <div className="flex-shrink-0 hidden md:block">
+        <ul className="flex flex-row">
+          {links.map((link) => (
+            <li
+              key={link.id}
+              className="block ml-4 text-black text-gray-500 dark:text-white nav"
+            >
+              <Link href={link.to}>
+                <a>{link.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Mobile Navbar */}
+      <div className="block md:hidden">
+      
+        <Dropdown label="Menu">
+          {links.map((link) => (
+            <Dropdown.Item key={link.id}>
+              <Link href={link.to}>
+                <a>{link.name}</a>
+              </Link>
+            </Dropdown.Item>
+          ))}
+       </Dropdown>
+      </div>
+    </>
+  );
+};
+
 
 export default NavBar
 
