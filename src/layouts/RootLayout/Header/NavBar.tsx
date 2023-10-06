ed0731e8dbbc7abde4978cd1ce039b158bb56025
Fairuz Ikbar <82931694+fairuzikbar@users.noreply.github.com>
2023-10-06 17:08:55 +0700

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -60,15 +60,15 @@ return (
 
 export default NavBar
 
-const StyledWrapper = styled.div`
-  flex-shrink: 0;
-  ul {
-    display: flex;
-    flex-direction: row;
-    li {
-      display: block;
-      margin-left: 1rem;
-      color: ${({ theme }) => theme.colors.gray11};
-    }
-  }
-`
+// const StyledWrapper = styled.div`
+//   flex-shrink: 0;
+//   ul {
+//     display: flex;
+//     flex-direction: row;
+//     li {
+//       display: block;
+//       margin-left: 1rem;
+//       color: ${({ theme }) => theme.colors.gray11};
+//     }
+//   }
+// `
